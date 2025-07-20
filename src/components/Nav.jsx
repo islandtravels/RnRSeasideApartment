1c3b44574fc3348dc1d2c645b5a1bd12ae977f31
Drei Abmab <dreiabmab@gmail.com>
2025-07-21 01:19:29 +0800

    Added Layout

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header>
+        <img src="" alt="" />
+        <nav>
+            <ul>
+                <li>Facilities</li>
+                <li>About</li>
+                <li>Find Us</li>
+                <li>FAQ</li>
+                <li>Book</li>
+            </ul>
+        </nav>
+        <a href="mailto:islandtravels.ph@gmail.com" target='_blank'>Contact Us</a>
+    </header>
+  )
+}
+
+export default Nav
