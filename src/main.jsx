64ba46e6594b4a9a9f092cc7260755128b6af914
Drei Abmab <dreiabmab@gmail.com>
2025-07-21 01:24:32 +0800

    Another commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
+// N
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
