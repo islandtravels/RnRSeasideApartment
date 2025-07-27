725b1e3ee9de7bdf51125f3681a15aa1b88ada5b
Drei Abmab <dreiabmab@gmail.com>
2025-07-27 11:58:48 +0800

    Updated Navbar

---
+++ src/assets/img/index.js
@@ -0,0 +1,60 @@
+export { default as bg } from './bg.jpg';
+export { default as close } from './close.png';
+export { default as icon } from './icon.png';
+export { default as loading } from './loading.gif';
+export { default as LogoIconMain } from './Logo-icon-main.svg';
+export { default as LogoIcon } from './Logo-icon.svg';
+export { default as next } from './next.png';
+export { default as prev } from './prev.png';
+
+// About images
+export { default as about_395778359 } from './about/395778359.jpg';
+export { default as about_img2 } from './about/img2.jpg';
+export { default as about_img3 } from './about/img3.jpg';
+
+// Check us images
+export { default as checkus_agoda } from './check us/agoda.svg';
+export { default as checkus_airbnb } from './check us/airbnb.svg';
+export { default as checkus_airbnbScore } from './check us/airbnbScore.svg';
+export { default as checkus_bookingcom } from './check us/bookingcom.svg';
+
+// Facilities images
+export { default as fac_bedSheets } from './fac/bed-sheets.png';
+export { default as fac_fireAlarm } from './fac/fire-alarm.png';
+export { default as fac_smokeDetector } from './fac/smoke-detector.png';
+
+// Gallery images
+export { default as gallery_img1 } from './gallery/img1.jpg';
+export { default as gallery_img2 } from './gallery/img2.jpg';
+export { default as gallery_img3 } from './gallery/img3.jpg';
+export { default as gallery_img4 } from './gallery/img4.jpg';
+export { default as gallery_img5 } from './gallery/img5.jpg';
+export { default as gallery_img6 } from './gallery/img6.jpg';
+export { default as gallery_img7 } from './gallery/img7.jpg';
+export { default as gallery_img8 } from './gallery/img8.jpg';
+export { default as gallery_img9 } from './gallery/img9.jpg';
+export { default as gallery_img10 } from './gallery/img10.jpg';
+export { default as gallery_img11 } from './gallery/img11.jpg';
+export { default as gallery_img12 } from './gallery/img12.jpg';
+export { default as gallery_img13 } from './gallery/img13.jpg';
+
+// Hero images
+export { default as hero_bg1 } from './hero/bg-1.png';
+export { default as hero_bg2 } from './hero/bg-2.jpg';
+
+// links
+export { default as links_agoda } from './links/agoda.svg';
+export { default as links_airbnb } from './links/airbnb.svg';
+export { default as links_bookingcom } from './links/bookingcom.svg';
+
+// Tesitimonial
+export { default as testimonial_fb } from './testimonial/fb.svg.png';
+export { default as testimonial_Kim } from './testimonial/Kim.JPG';
+export { default as testimonial_Malou } from './testimonial/Malou.JPG';
+export { default as testimonial_next } from './testimonial/next.svg';
+export { default as testimonial_prev } from './testimonial/prev.svg';
+export { default as testimonial_quote } from './testimonial/quote.svg';
+export { default as testimonial_Ting } from './testimonial/Ting.JPG';
+export { default as testimonial_Vidal } from './testimonial/Vidal.jpg';
+export { default as testimonial_wonder } from './testimonial/wonder.jpg';
+
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import "../css/nav.css"
+import { LogoIconMain } from '../assets/img'
 
 const Nav = () => {
   return (
     <header>
-        <img src="" alt="" />
+      <div className="nav">
+        <div className="img_con">
+          <img src={ LogoIconMain } alt="" />
+        </div>
         <nav>
             <ul>
-                <li>Facilities</li>
+                <li>Facilties</li>
                 <li>About</li>
                 <li>Find Us</li>
                 <li>FAQ</li>
                 <li>Book</li>
             </ul>
         </nav>
-        <a href="mailto:islandtravels.ph@gmail.com" target='_blank'>Contact Us</a>
+        <div className="contact">
+          <a href="mailto:islandtravels.ph@gmail.com" target='_blank'>Contact Us</a>
+          <i class="fa-solid fa-inbox" style={{color:"#0077B6"}}></i>
+        </div>
+      </div>
     </header>
   )
 }
